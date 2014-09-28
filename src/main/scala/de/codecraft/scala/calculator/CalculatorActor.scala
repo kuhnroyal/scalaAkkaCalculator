f7a5b45d51dbbc385f3c82bd6c89a4c40d5cd5ed
Thomas Fiedler <t.fiedler@codecraft.de>
2014-09-28 15:00:53 +0200

    add case classes for messages

--- src/main/scala/de/codecraft/scala/calculator/CalculatorActor.scala
+++ src/main/scala/de/codecraft/scala/calculator/CalculatorActor.scala
@@ -2,8 +2,26 @@ package de.codecraft.scala.calculator
 
 import akka.actor.{Props, Actor}
 
-object RacketActor {
+object CalculatorActor {
   def props = Props(new CalculatorActor)
+  // Operations
+  case object Addition
+  case object Subtraction
+  case object Multiplication
+  case object Division
+  case object Equals
+
+  // Numbers
+  case object One
+  case object Two
+  case object Three
+  case object Four
+  case object Five
+  case object Six
+  case object Seven
+  case object Eight
+  case object Nine
+  case object Zero
 }
 
 class CalculatorActor extends Actor {
