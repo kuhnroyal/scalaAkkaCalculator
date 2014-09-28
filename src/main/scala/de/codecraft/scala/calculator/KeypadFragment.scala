aeec05aa07cb61170efb5669bed59d18089489a6
Thomas Fiedler <t.fiedler@codecraft.de>
2014-09-28 16:36:26 +0200

    adjust keypad

--- src/main/scala/de/codecraft/scala/calculator/KeypadFragment.scala
+++ src/main/scala/de/codecraft/scala/calculator/KeypadFragment.scala
@@ -26,27 +26,10 @@ class KeypadFragment extends AkkaFragment with Contexts[AkkaFragment] {
   def send(m: CalculatorActor.Message) = actor.foreach(_ ! m)
 
   def addButton(m: CalculatorActor.Message) = {
-    m match {
-      case One => w[Button] <~ text("1") <~ On.click(Ui(send(m)))
-      case Two => w[Button] <~ text("2") <~ On.click(Ui(send(m)))
-      case Three => w[Button] <~ text("3") <~ On.click(Ui(send(m)))
-      case Four => w[Button] <~ text("4") <~ On.click(Ui(send(m)))
-      case Five => w[Button] <~ text("5") <~ On.click(Ui(send(m)))
-      case Six => w[Button] <~ text("6") <~ On.click(Ui(send(m)))
-      case Seven => w[Button] <~ text("7") <~ On.click(Ui(send(m)))
-      case Eight => w[Button] <~ text("8") <~ On.click(Ui(send(m)))
-      case Nine => w[Button] <~ text("9") <~ On.click(Ui(send(m)))
-      case Zero => w[Button] <~ text("0") <~ On.click(Ui(send(m)))
-      case Clear => w[Button] <~ text("C") <~ On.click(Ui(send(m)))
-      case Addition => w[Button] <~ text("+") <~ On.click(Ui(send(m)))
-      case Subtraction => w[Button] <~ text("-") <~ On.click(Ui(send(m)))
-      case Multiplication => w[Button] <~ text("*") <~ On.click(Ui(send(m)))
-      case Division => w[Button] <~ text("/") <~ On.click(Ui(send(m)))
-      case Equals => w[Button] <~ text("=") <~ On.click(Ui(send(m)))
-    }
+    w[Button] <~ text(m.symbol) <~ On.click(Ui(send(m)))
   }
 
-  def layout(implicit ctx: ActivityContext) = {
+  def layout = {
     l[LinearLayout](
       l[LinearLayout](
         addButton(One),
